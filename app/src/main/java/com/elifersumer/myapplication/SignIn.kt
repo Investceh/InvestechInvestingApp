cb2696c7b4137678d884771972b9776ade0e131c
Erdi Bayır <erdi.bayir2017@gtu.edu.tr>
2021-08-27 15:39:34 +0300

    Navbar

--- app/src/main/java/com/elifersumer/myapplication/SignIn.kt
+++ app/src/main/java/com/elifersumer/myapplication/SignIn.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_sign_in.*
 
 
 class SignIn : AppCompatActivity() {
@@ -11,6 +12,10 @@ class SignIn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+        loginButton.setOnClickListener {
+            val intent = Intent(this,navbar::class.java)
+            startActivity(intent)
+        }
     }
 
     fun signup_OnClick(view:View){
--- app/src/main/java/com/elifersumer/myapplication/navbar.kt
+++ app/src/main/java/com/elifersumer/myapplication/navbar.kt
@@ -2,10 +2,36 @@ package com.elifersumer.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.elifersumer.myapplication.fragments.*
+import kotlinx.android.synthetic.main.activity_navbar.*
 
 class navbar : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navbar)
+        val karsilamaFragment = KarsilamaFragment()
+        val piyasaFragment = PiyasaFragment()
+        val transferFragment = TransferFragment()
+        val emirFragment = EmirFragment()
+        val digerFragment = DigerFragment()
+
+        makeCurrentFragment(karsilamaFragment)
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> makeCurrentFragment(karsilamaFragment)
+                R.id.piyasa -> makeCurrentFragment(piyasaFragment)
+                R.id.transfer -> makeCurrentFragment(transferFragment)
+                R.id.emir -> makeCurrentFragment(emirFragment)
+                R.id.diger -> makeCurrentFragment(digerFragment)
+
+            }
+            true
+        }
     }
+    private fun makeCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper,fragment)
+            commit()
+        }
 }
