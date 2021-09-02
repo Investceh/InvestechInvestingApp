3e3f66006d3b38d11a91a57953da591e49e47f83
Elif Ersümer <elifersumer@gmail.com>
2021-09-02 10:50:41 +0300

    Firebase Authentication (Sign In & Sign Up)

--- app/src/main/java/com/elifersumer/myapplication/SignIn.kt
+++ app/src/main/java/com/elifersumer/myapplication/SignIn.kt
@@ -4,18 +4,42 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import com.elifersumer.myapplication.databinding.ActivitySignInBinding
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_sign_in.*
 import kotlinx.serialization.ExperimentalSerializationApi
 
 
 
 class SignIn : AppCompatActivity() {
+    private lateinit var binding:ActivitySignInBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
-        loginButton.setOnClickListener {
-            val intent = Intent(this,navbar::class.java)
-            startActivity(intent)
+        binding=ActivitySignInBinding.inflate(layoutInflater)
+        val view=binding.root
+        setContentView(view)
+
+        auth = FirebaseAuth.getInstance()
+
+
+    }
+
+    fun signInClicked(view: View){
+        val email=binding.txtEmail.text.toString()
+        val password=binding.textViewPassw.text.toString()
+
+        if( email.equals("") || password.equals("")){
+            Toast.makeText(this,"Lütfen email ve şifre giriniz.", Toast.LENGTH_LONG).show()
+        }else{
+            auth.signInWithEmailAndPassword(email,password).addOnSuccessListener {
+                val intent = Intent(this,navbar::class.java)
+                startActivity(intent)
+            }.addOnFailureListener {
+                Toast.makeText(this,it.localizedMessage, Toast.LENGTH_LONG).show()
+            }
         }
     }
 
--- app/src/main/java/com/elifersumer/myapplication/SignUp.kt
+++ app/src/main/java/com/elifersumer/myapplication/SignUp.kt
@@ -1,11 +1,44 @@
 package com.elifersumer.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.elifersumer.myapplication.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUp : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding:ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding=ActivitySignUpBinding.inflate(layoutInflater)
+        val view=binding.root
+        setContentView(view)
+
+        auth=FirebaseAuth.getInstance()
+
+    }
+
+    fun signUpClicked(view: View){
+        val name=binding.name.text.toString()
+        val email=binding.email.text.toString()
+        val tckn=binding.tckn.text.toString()
+        val phone=binding.phoneNum.text.toString()
+        val password=binding.password.text.toString()
+
+        if(name.equals("") || email.equals("") || tckn.equals("") || password.equals("") || phone.equals("")){
+            Toast.makeText(this,"Kayıt olmak için boş alanları doldurunuz.",Toast.LENGTH_LONG).show()
+        }else{
+            auth.createUserWithEmailAndPassword(email,password).addOnSuccessListener {
+                val intent = Intent(this@SignUp , SignIn::class.java)
+                startActivity(intent)
+                finish()
+            }.addOnFailureListener {
+                Toast.makeText(this,it.localizedMessage,Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
