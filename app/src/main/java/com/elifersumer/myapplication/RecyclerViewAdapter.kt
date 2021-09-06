a0d78b6ea49fe0fb8dda643a37630a801bee34fe
ozar <ozaromer@yahoo.com.tr>
2021-09-06 15:52:40 +0300

    protfoy clickable

--- app/src/main/java/com/elifersumer/myapplication/RecyclerViewAdapter.kt
+++ app/src/main/java/com/elifersumer/myapplication/RecyclerViewAdapter.kt
@@ -9,13 +9,24 @@ import androidx.recyclerview.widget.RecyclerView
 import com.elifersumer.myapplication.R
 import com.elifersumer.myapplication.fragments.hisseler
 
-class RecyclerViewAdapter(var hisse_list : ArrayList<hisseler>):
+class RecyclerViewAdapter(var hisse_list : ArrayList<hisseler>,
+    var clickListner: OnRecyclerItemClickListner):
     RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
     {
         val tourName=view.findViewById<TextView>(R.id.title_tv)
         val photo=view.findViewById<ImageView>(R.id.imagev)
         val cost=view.findViewById<TextView>(R.id.cost)
+        fun initialize(item: hisseler, action:OnRecyclerItemClickListner)
+        {
+            tourName.text = item.name
+            cost.text = item.cost
+            photo.setImageResource(item.photo)
+
+            itemView.setOnClickListener{
+                action.onItemClick(item, adapterPosition)
+            }
+        }
     }
 
     override fun onCreateViewHolder(
@@ -30,6 +41,7 @@ class RecyclerViewAdapter(var hisse_list : ArrayList<hisseler>):
         holder.tourName.text=hisse_list[position].name
         holder.photo.setImageResource(hisse_list[position].photo)
         holder.cost.text= hisse_list[position].cost
+        holder.initialize(hisse_list.get(position),clickListner)
     }
 
     override fun getItemCount(): Int {
@@ -38,3 +50,6 @@ class RecyclerViewAdapter(var hisse_list : ArrayList<hisseler>):
 
 }
 
+interface OnRecyclerItemClickListner{
+    fun onItemClick(item:hisseler,position: Int)
+}
--- app/src/main/java/com/elifersumer/myapplication/SignIn.kt
+++ app/src/main/java/com/elifersumer/myapplication/SignIn.kt
@@ -6,21 +6,10 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import com.elifersumer.myapplication.databinding.ActivitySignInBinding
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
-import kotlinx.android.synthetic.main.activity_sign_in.*
-import kotlinx.android.synthetic.main.activity_sign_up.*
-import kotlinx.serialization.ExperimentalSerializationApi
-
 
 
 class SignIn : AppCompatActivity() {
     private lateinit var binding:ActivitySignInBinding
-    private lateinit var auth: FirebaseAuth
-    private lateinit var firestore:FirebaseFirestore
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,35 +18,15 @@ class SignIn : AppCompatActivity() {
         val view=binding.root
         setContentView(view)
 
-        auth = FirebaseAuth.getInstance()
-        firestore = Firebase.firestore
-
-
-
-
 
     }
 
     fun signInClicked(view: View){
-        val email=binding.txtEmail.text.toString()
-        val password=binding.textViewPassw.text.toString()
 
-        if( email.equals("") || password.equals("")){
-            Toast.makeText(this,"Lütfen email ve şifre giriniz.", Toast.LENGTH_LONG).show()
-        }else{
-            auth.signInWithEmailAndPassword(email,password).addOnSuccessListener {
-                val intent = Intent(this,navbar::class.java)
-                startActivity(intent)
+        val intent = Intent(this,navbar::class.java)
+        startActivity(intent)
 
 
-
-
-
-
-            }.addOnFailureListener {
-                Toast.makeText(this,it.localizedMessage, Toast.LENGTH_LONG).show()
-            }
-        }
     }
     fun signup_OnClick(view:View){
         val intent = Intent(this,SignUp::class.java)
