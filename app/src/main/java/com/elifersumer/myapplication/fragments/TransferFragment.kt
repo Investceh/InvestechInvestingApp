770f4aad778ad5ba5f4b6d9a12ad8d04affcbac1
GokselOnal <73059230+GokselOnal@users.noreply.github.com>
2021-09-06 14:41:06 +0300

    aa

--- app/src/main/java/com/elifersumer/myapplication/fragments/TransferFragment.kt
+++ app/src/main/java/com/elifersumer/myapplication/fragments/TransferFragment.kt
@@ -26,9 +26,6 @@ class TransferFragment : Fragment() {
     lateinit var yatırım_vadesiz: RadioButton
 
     lateinit var oranlar: RadioGroup
-    lateinit var rg_25: RadioButton
-    lateinit var rg_50: RadioButton
-    lateinit var rg_75: RadioButton
     lateinit var rg_100: RadioButton
 
     lateinit var btn_tamam: Button
@@ -45,9 +42,6 @@ class TransferFragment : Fragment() {
         yatırım_vadesiz = view.findViewById(R.id.yatırım_vadesiz) as RadioButton
 
         oranlar = view.findViewById(R.id.oranlar) as RadioGroup
-        rg_25 = view.findViewById(R.id.rg_25) as RadioButton
-        rg_50 = view.findViewById(R.id.rg_50) as RadioButton
-        rg_75 = view.findViewById(R.id.rg_75) as RadioButton
         rg_100 = view.findViewById(R.id.rg_100) as RadioButton
 
         btn_tamam = view.findViewById(R.id.btn_tamam) as Button
@@ -56,46 +50,7 @@ class TransferFragment : Fragment() {
         yatırım_bilgi.text = cüzdan_yatırım.toString()
 
         miktar = view.findViewById(R.id.miktar) as EditText
-        rg_25.setOnClickListener(View.OnClickListener {
-            secilen_miktar_double = 0.0
-            if(!vadesiz_yatırım.isChecked && !yatırım_vadesiz.isChecked){
-                Toast.makeText(this@TransferFragment.requireActivity(),"Lütfen işlem seçiniz!",Toast.LENGTH_SHORT).show()
-            }else {
-                if (oranlar.checkedRadioButtonId != -1) {
-                    if (rg_25.isChecked && vadesiz_yatırım.isChecked)
-                        miktar.setText((cüzdan_vadesiz * 0.25).toString())
-                    else if (rg_25.isChecked && yatırım_vadesiz.isChecked)
-                        miktar.setText((cüzdan_yatırım * 0.25).toString())
-                }
-            }
-        })
-        rg_50.setOnClickListener(View.OnClickListener {
-            secilen_miktar_double = 0.0
-            if(!vadesiz_yatırım.isChecked && !yatırım_vadesiz.isChecked){
-                Toast.makeText(this@TransferFragment.requireActivity(),"Lütfen işlem seçiniz!",Toast.LENGTH_SHORT).show()
-            }else {
-                if (oranlar.checkedRadioButtonId != -1) {
-                    if (rg_50.isChecked && vadesiz_yatırım.isChecked)
-                        miktar.setText((cüzdan_vadesiz * 0.50).toString())
-                    else if (rg_50.isChecked && yatırım_vadesiz.isChecked)
-                        miktar.setText((cüzdan_yatırım * 0.50).toString())
-                }
-            }
-        })
 
-        rg_75.setOnClickListener(View.OnClickListener {
-            secilen_miktar_double = 0.0
-            if(!vadesiz_yatırım.isChecked && !yatırım_vadesiz.isChecked){
-                Toast.makeText(this@TransferFragment.requireActivity(),"Lütfen işlem seçiniz!",Toast.LENGTH_SHORT).show()
-            }else {
-                if (oranlar.checkedRadioButtonId != -1) {
-                    if (rg_75.isChecked && vadesiz_yatırım.isChecked)
-                        miktar.setText((cüzdan_vadesiz * 0.75).toString())
-                    else if (rg_75.isChecked && yatırım_vadesiz.isChecked)
-                        miktar.setText((cüzdan_yatırım * 0.75).toString())
-                }
-            }
-        })
 
         rg_100.setOnClickListener(View.OnClickListener {
             secilen_miktar_double = 0.0
@@ -110,8 +65,19 @@ class TransferFragment : Fragment() {
                 }
             }
         })
-        btn_tamam.setOnClickListener(View.OnClickListener {
 
+        vadesiz_yatırım.setOnClickListener(View.OnClickListener {
+            oranlar.clearCheck()
+            miktar.setText("")
+        })
+
+        yatırım_vadesiz.setOnClickListener(View.OnClickListener {
+            oranlar.clearCheck()
+            miktar.setText("")
+        })
+
+        btn_tamam.setOnClickListener(View.OnClickListener {
+            //Toast.makeText(this@TransferFragment.requireActivity(),"ahahahahahaha",Toast.LENGTH_SHORT).show()
             //val drawable: Drawable?= ResourcesCompat.getDrawable(resources,R.drawable.button, null)
             //val drawable_selected: Drawable?= ResourcesCompat.getDrawable(resources,R.drawable.button_selected, null)
 
@@ -160,6 +126,7 @@ class TransferFragment : Fragment() {
                 }
             }
         })
+
         // Inflate the layout for this fragment
         return view
     }
