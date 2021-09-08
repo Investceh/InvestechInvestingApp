e5f74f8386e1f1ac859e395f7a88bb0955613f55
ozar <ozaromer@yahoo.com.tr>
2021-09-08 11:35:01 +0300

    page portfoyum updated

--- app/src/main/java/com/elifersumer/myapplication/Hisseler.kt
+++ app/src/main/java/com/elifersumer/myapplication/Hisseler.kt
@@ -1,3 +1,3 @@
 package com.elifersumer.myapplication.fragments
 
-data class hisseler(var name : String,var sh_name : String ,var photo : Int, var cost:String) {}
+data class hisseler(var name : String,var sh_name : String ,var photo : Int, var cost: Double, var tane : Int, var k_z : Double) {}
