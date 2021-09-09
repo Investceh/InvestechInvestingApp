7332a0b73b60eb2958710e9185db6aa339eb1ea8
ozar <ozaromer@yahoo.com.tr>
2021-09-09 14:53:17 +0300

    piyasa backend fixed

--- app/src/main/java/com/elifersumer/myapplication/LiveBorsa/Response/LiveBorsaData.kt
+++
@@ -1,9 +0,0 @@
-package com.elifersumer.myapplication.LiveBorsa.Response
-
-import androidx.lifecycle.MutableLiveData
-import com.elifersumer.myapplication.GetCustomerPortfolio.Response.KonsolideStock
-import com.elifersumer.myapplication.GetCustomerPortfolio.Response.Stock
-
-class LiveBorsaData {
-    var result : List<StockInfo>?=null
-}
