e63ed2ad46d9fa1ed687316bf16950f92935a4db
Erdi Bayır <erdi.bayir2017@gtu.edu.tr>
2021-09-10 23:25:56 +0300

    Bundle olayını yaptık.10 numara oldu lanet olsun mükkemmel bir şey bu

--- app/src/main/java/com/elifersumer/myapplication/fragments/EmirFragment.kt
+++ app/src/main/java/com/elifersumer/myapplication/fragments/EmirFragment.kt
@@ -1,93 +1,46 @@
 package com.elifersumer.myapplication.fragments
 
+import android.graphics.Color
+import android.graphics.Typeface
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.elifersumer.myapplication.CollectApi.CollectApiInstance
-import com.elifersumer.myapplication.HisseInfo
-import com.elifersumer.myapplication.LiveBorsa.Response.LiveBorsaResponse
-import com.elifersumer.myapplication.LiveBorsa.Response.StockInfo
-import com.elifersumer.myapplication.PiyasaData
+import com.elifersumer.myapplication.MessageEvent
 import com.elifersumer.myapplication.R
-import com.elifersumer.myapplication.RecyclerViewAdapterPiyasa
 import kotlinx.android.synthetic.main.fragment_emirgiris.*
 import kotlinx.android.synthetic.main.fragment_emirgiris.view.*
-import kotlinx.android.synthetic.main.fragment_piyasa.*
 import kotlinx.android.synthetic.main.fragment_piyasa.view.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import java.text.DecimalFormat
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
 
 
 class EmirFragment : Fragment() {
+    lateinit var messageTextView: EditText
     lateinit var alisbtn : RadioButton
     lateinit var satisbtn: RadioButton
     lateinit var tamamBtn: Button
     lateinit var adet: EditText
     lateinit var hisseler: AutoCompleteTextView
     lateinit var rg: RadioGroup
-    var nameList = ArrayList<String>()
-    lateinit var hisseInfo:MutableList<HisseInfo>
-    lateinit var t_alis : String
-    lateinit var t_satis : String
-    lateinit var t_miktar1:String
-    lateinit var t_miktar2:String
+    var messageToDisplay:String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view =  inflater.inflate(R.layout.fragment_emirgiris, container, false)
-        val alisFiyat = view.findViewById(R.id.val_alis) as TextView
-        val satisFiyat = view.findViewById(R.id.val_satis) as TextView
-        val mik_1Fiyat = view.findViewById(R.id.val_miktar1) as TextView
-        val mik_2Fiyat = view.findViewById(R.id.val_miktar2) as TextView
-
-        var retrofit= CollectApiInstance.getRetrofitObject()?.create(com.elifersumer.myapplication.LiveBorsa.Service.BorsaService::class.java)
-
-        var result : Call<LiveBorsaResponse> = retrofit!!.GetPostValue()
-
-        var borsaList:List<StockInfo>
-
-        result.enqueue(object : Callback<LiveBorsaResponse?> {
-            override fun onResponse(call: Call<LiveBorsaResponse?>?, response: Response<LiveBorsaResponse?>) {
-                var data = response.body()!!.GetData()
-                borsaList=data!!
-                for(stock in borsaList){
-                    nameList.add(stock.name!!)
-                    var satis = ((stock.price!!)*(stock.rate!! / 100)) + stock.price!!
-                    var satisString : String
-                    val df = DecimalFormat("#,##0.00")
-                    satisString = df.format(satis)
-                    var h1 = HisseInfo(stock.name!!,"10,542",stock.pricestr!!,satisString,"56,088")
-                    hisseInfo.add(h1)
-                }
-
-                var arrayAdapter = ArrayAdapter(requireContext(),R.layout.dropdown_item, nameList)
-                view.autoCompleteTextView.setAdapter(arrayAdapter)
-                var name = autoCompleteTextView.text.toString()
-                for(hisse in hisseInfo){
-                    if(hisse.Name==name) {
-                        t_alis = hisse.Alis!!
-                        t_satis = hisse.Satis!!
-                        t_miktar1 = hisse.Miktar1!!
-                        t_miktar2 = hisse.Miktar2!!
-                    }
-                }
-                alisFiyat.text=t_alis
-                satisFiyat.text=t_satis
-                mik_1Fiyat.text=t_miktar1
-                mik_2Fiyat.text=t_miktar2
-            }
-            override fun onFailure(call: Call<LiveBorsaResponse?>?, t: Throwable?) {
-
-            }
-        })
+        val args = this.arguments
+        val satisData = args?.get("satis")
+        val alisData = args?.get("alis")
+        val isimData = args?.get("isim")
+        val alisOrSatis = args?.get("alisOrSatis")
+        Log.d("satis:",satisData.toString())
 
         alisbtn = view.findViewById(R.id.alisbtn) as RadioButton
         satisbtn = view.findViewById(R.id.satisbtn) as RadioButton
@@ -98,22 +51,48 @@ class EmirFragment : Fragment() {
 
         adet = view.findViewById(R.id.edtxt_adet) as EditText
 
-
+        val alisFiyat = view.findViewById(R.id.val_alis) as TextView
+        val satisFiyat = view.findViewById(R.id.val_satis) as TextView
         val fiyat = view.findViewById(R.id.edtxt_fiyat) as EditText
+
+        alisFiyat.text = alisData.toString()
+        satisFiyat.text = satisData.toString()
 
         rg = view.findViewById(R.id.daily_weekly_button_view) as RadioGroup
 
         hisseler = view.findViewById(R.id.autoCompleteTextView) as AutoCompleteTextView
-
-        fiyat.setText(satisFiyat.text)
+        hisseler.setText(isimData.toString())
+        if (alisOrSatis == "satis"){
+            fiyat.setText(alisData.toString())
+            satisbtn.isChecked = true
+        }
+        else{
+            fiyat.setText(satisData.toString())
+            alisbtn.isChecked = true
+        }
 
         alisbtn.setOnClickListener(View.OnClickListener {
-            //Toast.makeText(this@EmirFragment.requireActivity(),"Lütfen işlem seçiniz!", Toast.LENGTH_SHORT).show()
+            //Toast.makeText(this@EmirFragment.requireActivity(),"AŞKSIN ", Toast.LENGTH_SHORT).show()
+            alisbtn.textSize = 16.toFloat()
+            //alisbtn.setTextColor(Color.parseColor("#f5f5f5"))
+            alisbtn.setTypeface(null, Typeface.BOLD)
+
+
+            satisbtn.textSize = 15.toFloat()
+            satisbtn.setTypeface(null, Typeface.NORMAL)
+
 
             fiyat.setText(satisFiyat.text)
         })
 
         satisbtn.setOnClickListener(View.OnClickListener {
+
+            satisbtn.textSize = 16.toFloat()
+            //alisbtn.setTextColor(Color.parseColor("#f5f5f5"))
+            satisbtn.setTypeface(null, Typeface.BOLD)
+
+            alisbtn.textSize = 15.toFloat()
+            alisbtn.setTypeface(null, Typeface.NORMAL)
 
             fiyat.setText(alisFiyat.text)
         })
@@ -133,7 +112,6 @@ class EmirFragment : Fragment() {
                 fiyat.setText(x.toString())
             }
         })
-
 
 
         var input_islem_tipi = "Alış"
@@ -168,15 +146,14 @@ class EmirFragment : Fragment() {
         }
 
 
-
-        //val hisseler = resources.getStringArray(R.array.hisseler)
+        val hisseler = resources.getStringArray(R.array.hisseler)
         val fiyatTipleri = resources.getStringArray(R.array.fiyatTipleri)
         val sureTipleri = resources.getStringArray(R.array.sureTipleri)
-
+        var arrayAdapter = ArrayAdapter(requireContext(),R.layout.dropdown_item, hisseler)
         var arrayAdapter2 = ArrayAdapter(requireContext(),R.layout.dropdown_item, fiyatTipleri)
         var arrayAdapter3 = ArrayAdapter(requireContext(), R.layout.dropdown_item, sureTipleri)
 
-
+        view.autoCompleteTextView.setAdapter(arrayAdapter)
         view.autoCompleteTextViewFiyatTipi.setAdapter(arrayAdapter2)
         view.autoCompleteTextViewSureTipi.setAdapter(arrayAdapter3)
 
@@ -185,7 +162,25 @@ class EmirFragment : Fragment() {
         return view.rootView
     }
 
+    override fun onStart() {
+        super.onStart()
+        EventBus.getDefault().register(this)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        EventBus.getDefault().unregister(this)
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    fun onMessageEvent(event:MessageEvent){
+        messageToDisplay = event.message
+        Log.d("deneme1:",event.message)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
+
+
 }
