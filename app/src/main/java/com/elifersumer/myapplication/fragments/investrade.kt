d473ddcf27989dac3c832bf1f021f1eb76d5492c
Ozar Uncu <ozaromer@yahoo.com.tr>
2021-09-16 18:53:27 +0300

    invest trade added

--- app/src/main/java/com/elifersumer/myapplication/fragments/investrade.kt
+++ app/src/main/java/com/elifersumer/myapplication/fragments/investrade.kt
@@ -14,6 +14,8 @@ class investrade : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_investrade, container, false)
     }
