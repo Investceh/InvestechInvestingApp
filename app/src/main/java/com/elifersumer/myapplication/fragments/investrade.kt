0da92c169044e4a6cc74e7e3d05e52161eb99dc0
Ozar Uncu <ozaromer@yahoo.com.tr>
2021-09-16 19:10:23 +0300

    investTrade

--- app/src/main/java/com/elifersumer/myapplication/fragments/investrade.kt
+++ app/src/main/java/com/elifersumer/myapplication/fragments/investrade.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import com.elifersumer.myapplication.R
+import kotlinx.android.synthetic.main.fragment_emirgiris.view.*
+import kotlinx.android.synthetic.main.fragment_investrade.view.*
 
 
 class investrade : Fragment() {
@@ -14,6 +17,19 @@ class investrade : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_investrade, container, false)
+        val arrayname = arrayOf("BIST100","BIST030","USD/TRY","EUR/TRY","EUR/USD","AKBNK")
+        var arrayAdapter = ArrayAdapter(requireContext(),R.layout.dropdown_item, arrayname)
+        view.Enstruman.setAdapter(arrayAdapter)
+
+        val arrayname2 = arrayOf("Model1","Model2","Model3")
+        var arrayAdapter2 = ArrayAdapter(requireContext(),R.layout.dropdown_item, arrayname2)
+        view.Model.setAdapter(arrayAdapter2)
+
+        val arrayname3 = arrayOf("%0","%25","%50","%75","%100")
+        var arrayAdapter3 = ArrayAdapter(requireContext(),R.layout.dropdown_item, arrayname3)
+        view.portfoyAgr.setAdapter(arrayAdapter3)
+
 
 
         // Inflate the layout for this fragment
